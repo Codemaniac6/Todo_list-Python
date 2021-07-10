ac411f2148e0cc15560bb4df6fb69dda9ef1fdad
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2021-07-11 00:34:42 +0100

    created Update, Delete and Create view and template

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import TaskListView, TaskDetailView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
-    path('task/<str:pk>', TaskDetailView.as_view(), name='task_detail')
+    path('task/<str:pk>', TaskDetailView.as_view(), name='task_detail'),
+    path('task-create/', TaskCreateView.as_view(), name='task-create'),
+    path('task-edit/<str:pk>', TaskUpdateView.as_view(), name='task-edit'),
+    path('task-delete/<str:pk>', TaskDeleteView.as_view(), name='task-delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -17,3 +18,23 @@ class TaskDetailView(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'todo/task_detail.html'
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'todo/task_create.html'
+    success_url = reverse_lazy('task_list')
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'todo/task_create.html'
+    success_url = reverse_lazy('task_list')
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('task_list')
